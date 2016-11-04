849780ad11fa70eccd9d09228f005f02bb4d9ab1
mike <michael.valenciav@gmail.com>
2016-11-04 16:32:21 -0500

    Applicant stored in cookie

--- src/app/applicant.ts
+++ src/app/applicant.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {CookieService} from 'angular2-cookie/core';
 
 export class Applicant {
   public firstname?: string;
@@ -11,9 +12,17 @@ export class Applicant {
 
 @Component({
   selector: 'reviewer-app',
-  template: require('./applicant.html')
+  template: require('./applicant.html'),
+  providers: [CookieService]
 })
 export class ApplicantFormComponent {
   title = 'Current User';
   applicant: Applicant = {};
+  constructor(private _cookieService: CookieService) {}
+
+  onSubmit(): void {
+    //console.log(this._cookieService.get('current_applicant'));
+    this._cookieService.putObject('current_applicant', this.applicant);
+    //console.log(this._cookieService.get('current_applicant'));
+  };
 }
